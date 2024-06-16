5e992cfeada1b7b955fb51aa320119c9e0158dfc
JongHyeon Park <67833043+Jshman@users.noreply.github.com>
2024-06-16 18:02:14 +0900

    문장 수정
    문장 수정을 통해 정확한 메시지 전달
    복구 금액 --- 필요한 이익률

--- simulator/inArmy/stock/script.js
+++ simulator/inArmy/stock/script.js
@@ -7,13 +7,18 @@ function calculateRequiredProfit() {
     return;
   }
 
-  var remainingAmount = initialInvestment * (1 - lossPercentage / 100);
+  var lossAmount = initialInvestment * (lossPercentage / 100);
+  var remainingAmount = initialInvestment - lossAmount;
   var requiredProfitPercentage = (initialInvestment / remainingAmount - 1) * 100;
 
+  var lossAmountElement = document.getElementById('lossAmountValue');
+  lossAmountElement.textContent = `${lossAmount.toFixed(2)} 원`;
+
   var resultElement = document.getElementById('result');
-  resultElement.innerHTML = `손해를 본 비율(<span class="highlight">${lossPercentage}%</span>)로 원금을 회수하기 위해 필요한 이익률: <span class="highlight">${requiredProfitPercentage.toFixed(2)}%</span>`;
+  resultElement.innerHTML = `복구 금액(${initialInvestment.toFixed(2) - lossAmount.toFixed(2)})으로 원금을 회수하기 위해 필요한 이익률: <span class="highlight">${requiredProfitPercentage.toFixed(2)}%</span>`;
 }
 
 function setLossPercentage(lossPercentage) {
   document.getElementById('lossPercentage').value = lossPercentage;
+  calculateRequiredProfit();
 }
